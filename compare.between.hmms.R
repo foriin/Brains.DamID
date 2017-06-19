36d61a091a2d3644bb5c419fffe5442cb1c9f003
foriin <liartom@mail.ru>
2017-06-19 14:05:54 +0300

    added comparison between domains

--- compare.between.hmms.R
+++ compare.between.hmms.R
@@ -8,6 +8,7 @@ sum_bed <- function(df){
                                   "2RHet", "3LHet", "3RHet", "XHet", "YHet"),
                         "len" = c(23011544, 21146708, 24543557, 27905053, 1351857,
                                   22422827, 368872, 3288761, 2555491, 2517507, 204112, 347038))
+  chr.len$chr <- paste0("chr", chr.len$chr)
   df %>% 
     mutate(len = end - start) %>% 
     group_by(chr) %>% 
@@ -16,19 +17,21 @@ sum_bed <- function(df){
     mutate(chrlen = chr.len$len[grepl(paste0("^", chr, "$"), chr.len$chr)])
 }
 
-setwd("~/IMG/Projects/HP1.Lamin.Polycomb.DNA.contacts.Effect.on.expression/DamID-seq.HP1.PC.Lam.WBr.Nrn.Glia.Fb/02.04.17_all_samples_300/hmm3/")
+setwd("~/IMG/Projects/HP1.Lamin.Polycomb.DNA.contacts.Effect.on.expression/DamID-seq.HP1.PC.Lam.WBr.Nrn.Glia.Fb/02.04.17_all_samples_300/BioHMM2/intersected/")
 
 beds <- new.env()
 for (i in dir()[grepl("bed", dir())]){
-  assign(sub("\\.bed", "", i), fread(i), envir = beds)
+  assign(sub("\\.domains.intersected.bed", "", i), fread(i, skip = 1), envir = beds)
 }
 rm(i)
 
-chr.len <- data.frame("chr" = c("2L", "2R", "3L", "3R", "4", "X", "2LHet",
-                                "2RHet", "3LHet", "3RHet", "XHet", "YHet"),
-                      "len" = c(23011544, 21146708, 24543557, 27905053, 1351857,
-                                22422827, 368872, 3288761, 2555491, 2517507, 204112, 347038)
-)
+# chr.len <- data.frame("chr" = c("2L", "2R", "3L", "3R", "4", "X", "2LHet",
+#                                 "2RHet", "3LHet", "3RHet", "XHet", "YHet"),
+#                       "len" = c(23011544, 21146708, 24543557, 27905053, 1351857,
+#                                 22422827, 368872, 3288761, 2555491, 2517507, 204112, 347038)
+# )
+# 
+
 
 comparisons <- new.env()
 
@@ -49,7 +52,8 @@ for (i in 1:(length(ls(envir = beds)) - 1)){
     y <- merge(ab, sum_bed(df_intersected), by = "chr") %>% 
       mutate("i/x" = round(dom_len / dom_len.x, digits=3),
              "i/y" = round(dom_len / dom_len.y, digits=3),
-             coverage = round(dom_len / chrlen, digits=3))
+             coverage = round(dom_len / chrlen, digits=3)) %>% 
+      dplyr::rename(dom_len.i = dom_len) #bitch-ass s4vectors package hid dplyr rename function
     assign(paste0(x.name, ".vs.", compto.name), y, envir = comparisons)
     return()
       
@@ -60,8 +64,27 @@ dir.create("Comparisons")
 setwd("Comparisons/")
 
 for (i in ls(envir = comparisons)){
+  
+  sumry.eu = get(i, envir = comparisons) %>% 
+    filter(!grepl("Het|4", chr)) %>% 
+    summarise_each(funs(sum), dom_len.x, dom_len.y, dom_len.i, chrlen) %>% 
+    mutate("i/x total" = round(dom_len.i / dom_len.x, digits = 3), "i/y total" = round(dom_len.i / dom_len.y, digits = 3),
+           "coverage x" = round(dom_len.x / chrlen, digits = 3),
+           "coverage y" = round(dom_len.y / chrlen, digits = 3),
+           "coverage i" = round(dom_len.i / chrlen, digits = 3))
+  sumry.het = get(i, envir = comparisons) %>% 
+    filter(grepl("Het|4", chr)) %>% 
+    summarise_each(funs(sum), dom_len.x, dom_len.y, dom_len.i, chrlen) %>% 
+    mutate("i/x total" = round(dom_len.i / dom_len.x, digits = 3), "i/y total" = round(dom_len.i / dom_len.y, digits = 3),
+           "coverage x" = round(dom_len.x / chrlen, digits = 3),
+           "coverage y" = round(dom_len.y / chrlen, digits = 3),
+           "coverage i" = round(dom_len.i / chrlen, digits = 3))
   write.table(get(i, envir = comparisons), paste0(i, ".csv"), quote = F, sep = ";", dec = ",", row.names = F)
-}
+  cat("Euchromatin summary\n", file = paste0(i, ".csv"), append = T)
+  write.table(sumry.eu, paste0(i, ".csv"), quote = F, sep = ";", dec = ",", row.names = F, append = T)
+  cat("Heterochromatin summary\n", file = paste0(i, ".csv"), append = T)
+  write.table(sumry.het, paste0(i, ".csv"), quote = F, sep = ";", dec = ",", row.names = F, append = T)
+  }
 
 
 BR.HP1 %>% 
